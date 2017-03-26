5f199e594783a9aa0c0baa4bf9d47cb2a3d12c90
jkwebco <jkwebdesignco@gmail.com>
2017-03-26 10:38:12 -0400

    Add files via upload

---
+++ tweet.py
@@ -0,0 +1,20 @@
+import got
+
+def receiveBuffer(tweets):
+	for t in tweets:
+		print ('\n')
+		print ('time: %s' % t.date.strftime("%Y-%m-%d %H:%M"))
+		print "Tweet: %s" % t.text
+	print ('\n')
+	print ('More %d printed to screen...\n' % len(tweets))
+
+variable = ""
+while variable != 'quit':
+
+	variable = raw_input('Lookup last 10 tweets on something, type something in: ')
+	tweetCriteria = got.manager.TweetCriteria().setQuerySearch(variable).setMaxTweets(10)
+	got.manager.TweetManager.getTweets(tweetCriteria, receiveBuffer)
+	
+		
+
+
