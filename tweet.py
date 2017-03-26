5eb3acdb69e773d52734b1f525bb94cf4d096df8
jkwebco <jkwebdesignco@gmail.com>
2017-03-26 11:49:50 -0400

    Update tweet.py
    added sentiment analysis, i.e. pos, neg, or neutral

--- tweet.py
+++ tweet.py
@@ -1,10 +1,24 @@
 import got
+from textblob import TextBlob
 
 def receiveBuffer(tweets):
 	for t in tweets:
 		print ('\n')
 		print ('time: %s' % t.date.strftime("%Y-%m-%d %H:%M"))
 		print "Tweet: %s" % t.text
+		analysis = TextBlob(t.text)
+		#print(analysis)
+		# set sentiment
+		if analysis.sentiment.polarity > 0:
+			print("positive")
+		
+		elif analysis.sentiment.polarity == 0:
+			#print("neutral" , analysis)
+			print ("neutral")
+			
+		else:
+			print ("negative")
+		
 		print "permalink: %s" %t.permalink
 	print ('\n')
 	print ('More %d printed to screen...\n' % len(tweets))
